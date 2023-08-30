40d57faad6904ebbd9dbfcc9d6d23881fa74c8c7
MatiasLedesma <matias@activity.com.ar>
2023-08-30 17:29:41 -0300

    gatsby frontend with daisyUI tailwindcss

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from './config/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -19,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
